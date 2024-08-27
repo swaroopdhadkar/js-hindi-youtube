442d16f98839dc46b1f4260a617bb13f61907965
swaroopdhadkar <swaroopdhadkar@gmail.com>
2024-08-27 11:04:34 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const acccountId = 12345
+let accountEmail = "hchc@gmail.com"
+var accountPassword = "212121"
+acoountCity = "Hyderabad"
+
+accountEmail ="abab@gmail.com"
+
+console.log(accountEmail);
+
+console.table([acccountId, accountEmail, accountPassword, acoountCity])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+let age =18;
+let name = "hc"
+let state = null;
+
+console.log(typeof age);
+
+console.log(typeof state);
+
+console.log(typeof undefined);
+
+
+

c6d327c29ac01ffab14143cbee2dd88e11b6d84a
swaroopdhadkar <swaroopdhadkar@gmail.com>
2024-08-27 12:50:11 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "Swaroop"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+// Data types conversion into Number
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = "Swaroop"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+// Data types conversion into Boolean 1 = true , 0 => false
+// "" => false
+// "Swaroop" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+

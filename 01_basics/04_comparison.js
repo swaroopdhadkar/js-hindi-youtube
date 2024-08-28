b8b9f599c1661c61d03b41c5399144745511d2f5
swaroopdhadkar <swaroopdhadkar@gmail.com>
2024-08-28 11:43:29 +0000

    datatype summary

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,40 @@
+// console.log(2>1);
+// console.log(2 >=1);
+// console.log(2<1);
+// console.log(2 == 1);
+// console.log(2!=1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >= 0);
+// //console.log(null < 1);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined >= 0);
+// console.log(undefined <= 0);
+
+// Equality check and Comparisons are two differrent things in Javascript, they work differently
+
+console.log("2" == 2);
+console.log("2" === 2);
+
+// When you do comparisons between two different data types, you may not get the pridicted output.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ datatypes-summary.js
@@ -0,0 +1,44 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null , Undefined, Symbol, Bigint
+
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+
+let userEmail;
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+console.log(id == anotherId);
+
+//const BigNumber = 54752394213743224n
+
+const heros = ["shaktiman", "naagraj", "doga"];
+
+let myObj = {
+    name: "Swaroop",
+    age: 22,
+}
+
+const myFunction = function(){
+
+    console.log("Hello World");
+}
+
+
+console.log(typeof outsideTemp);
+
+console.log(typeof heros);
+
+
+
+
+
+// Reference (Non Primitive)
+
+// Arrays, Objects

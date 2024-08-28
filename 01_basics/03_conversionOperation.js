a095fed4e131b010660aac6f9338abf42d642d45
swaroopdhadkar <swaroopdhadkar@gmail.com>
2024-08-28 09:18:10 +0000

    More Vonversion and Operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,7 +27,62 @@ let someNumber = 33
 
 let stringNumber = String(someNumber)
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+/* Operation  */
+
+let value = 3
+
+let negValue = -value
+
+//console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Swaroop"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+
+// console.log("1" + 2 + 2);
+
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+//console.log(+true);
+//console.log(+"");
+
+let num1 , num2 , num3 ;
+
+num1 =num2 = num3 = 2 + 2
+
+//console.log(num1);
+
+let gamecounter = 100 ;
+++gamecounter;
+
+console.log(gamecounter);
+
+
+
+
+
+
+
+
+
+
 
 

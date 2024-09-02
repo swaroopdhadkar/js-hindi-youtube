fd4d9305a35106d4fca798b97585ff4b5ed577b3
swaroopdhadkar <swaroopdhadkar@gmail.com>
2024-09-02 12:45:14 +0000

    object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,18 @@
+// Singleton                                // Yeh apne tarah ka ek hi object he  // When we make an object through constructor , it is singleton
+
+// Object literals
+//Object.create
+
+const JsUser = {
+    name:"Hitesh",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser.email)
+console.log(JsUser[email])
+
+
